9074c90a39e90b5e260db4ba7da15f8bba38edc3
SamdenLepcha <sam.lepcha98@gmail.com>
2021-11-05 16:16:39 +0530

    Adding main toxicity model code.

---
+++ two_step_approach/main.py
@@ -0,0 +1,52 @@
+#Importing all Libraries
+import pandas as pd
+import numpy as np
+import librosa
+import os
+import torch
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+from simpletransformers.classification import ClassificationModel
+from sklearn.metrics import classification_report
+import warnings
+warnings.filterwarnings("ignore")
+
+#Initialising Paths
+
+source='../Testing/Input/test/'
+
+destination='../Testing/Output/'
+
+#Calling Transcribing Function
+get_transcripts(source,destination)
+
+#Importing Saved Transcripts
+
+data=pd.read_csv(destination+"Transcripts_txt",sep=';')
+
+data.columns = ["text", "labels"]
+
+#Running Locally Saved Toxicity Model
+
+cuda_available = torch.cuda.is_available()
+
+model = ClassificationModel(
+    "bert", "outputs/best_model", use_cuda=cuda_available
+)
+
+#Making predictions
+predictions=[]
+raw_outputs=[]
+
+for i in range(data.shape[0]):
+
+	temp_predictions, temp_raw_outputs = model.predict([data['text'].iloc[i]])
+
+	predictions.append(temp_predictions)
+	raw_outputs.append(temp_raw_outputs)
+
+data['Predictions']=predictions
+
+#Evaluating Results
+
+print(classification_report(data['labels'], data['Predictions']))
+
